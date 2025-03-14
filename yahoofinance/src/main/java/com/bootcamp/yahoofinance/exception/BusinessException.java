886885062f1a61dacb2bc351de8d7a747c279d39
pymephwan <pymephy@gmail.com>
2025-03-14 17:13:16 +0800

    Delete yahoofinance directory

--- yahoofinance/src/main/java/com/bootcamp/yahoofinance/exception/BusinessException.java
+++
@@ -1,5 +0,0 @@
-package com.bootcamp.yahoofinance.exception;
-
-public class BusinessException extends RuntimeException {
-  
-}
--- yahoofinance/src/main/java/com/bootcamp/yahoofinance/service/YahooService.java
+++
@@ -1,9 +0,0 @@
-package com.bootcamp.yahoofinance.service;
-
-import java.util.List;
-import com.bootcamp.yahoofinance.DTO.StockDTO;
-
-public interface YahooService {
-
-  List<StockDTO> getPrice(String stockCode);
-}
