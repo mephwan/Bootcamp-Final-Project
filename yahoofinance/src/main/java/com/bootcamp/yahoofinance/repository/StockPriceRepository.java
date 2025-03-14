c2cacd63e1f4458ecc327425ef7f99ee7815ab01
pymephwan <pymephy@gmail.com>
2025-03-14 13:23:37 +0800

    Delete yahoofinance directory

--- yahoofinance/src/main/java/com/bootcamp/yahoofinance/repository/StockPriceRepository.java
+++
@@ -1,8 +0,0 @@
-package com.bootcamp.yahoofinance.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.bootcamp.yahoofinance.entity.StockPriceEntity;
-
-public interface StockPriceRepository extends JpaRepository<StockPriceEntity, Long> {
-  
-}
