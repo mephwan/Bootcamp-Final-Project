703b197df025a4e46b706f9a7b70bb722f81aa38
mephy <mephwan@gmail.com>
2025-03-27 21:01:29 +0800

    update

--- Backend-PriceAPI/src/test/java/com/bootcamp/PriceAPI/PriceAPIApplicationTests.java
+++ Backend-PriceAPI/src/test/java/com/bootcamp/PriceAPI/PriceAPIApplicationTests.java
@@ -1,10 +1,10 @@
-package com.bootcamp.yahoofinance;
+package com.bootcamp.PriceAPI;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class YahoofinanceApplicationTests {
+class PriceAPIApplicationTests {
 
 	@Test
 	void contextLoads() {
